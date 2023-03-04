32cc6eb3b72de33325f9c108912885ec0c08cd05
buntueng <mr.buntueng@gmail.com>
2023-03-04 21:03:40 +0700

    add script to manage serial port data

--- active_test.py
+++ active_test.py
@@ -8,6 +8,9 @@ import yaml
 from yaml.loader import SafeLoader
 import logging
 import time
+import sys
+import serial
+import glob
 
 current_path = Path(__file__).resolve().parents[0]
 experiments_path = Path(current_path,'experiments')
@@ -36,6 +39,26 @@ def read_yml(file_path):
         logging.warning("file path is not here")
     return yml_params
 
+def list_serial_ports():
+   if sys.platform.startswith('win'):
+      ports = ['COM%s' % (i + 1) for i in range(2,20)]
+   elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+      # this excludes your current terminal "/dev/tty"
+      ports = glob.glob('/dev/tty[A-Za-z]*')
+   elif sys.platform.startswith('darwin'):
+      ports = glob.glob('/dev/tty.*')
+   else:
+      raise EnvironmentError('Unsupported platform')
+   result = []
+   for port in ports:
+      try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+      except (OSError, serial.SerialException):
+            pass
+   return result
+
 main_window = tk.Tk()
 screen_width = main_window.winfo_screenwidth()
 screen_height = main_window.winfo_screenheight()
@@ -53,6 +76,7 @@ test_frame = tk.Frame(main_window)
 
 message_label = tk.StringVar()
 timer_display = tk.StringVar()
+select_comport_var = tk.StringVar()
 # ===========================================================================================
 main_state = 0
 state_timer = None
@@ -60,6 +84,8 @@ start_time = None
 timeout_ms = 10000        # set timeout = 10 second
 test_param = None
 test_result = []
+
+serial_port = None
 
 def current_millis():
     return round(time.time() * 1000)
@@ -84,13 +110,22 @@ def start_button_pressed():
    global main_state
    global test_param
 
-   test_param_path = Path(current_path,"experiments")
-   test_param_path = Path(test_param_path,select_exp_option.get())
-   param_file_obj = open(test_param_path,"r",encoding="utf-8")
-   test_param = param_file_obj.readlines()
-   main_state = 0
-   show_test_frame()
-   run_state()
+   if select_comport_option.get() != "" and select_exp_option.get() != "":
+      test_param_path = Path(current_path,"experiments")
+      test_param_path = Path(test_param_path,select_exp_option.get())
+      param_file_obj = open(test_param_path,"r",encoding="utf-8")
+      test_param = param_file_obj.readlines()
+      main_state = 0
+      show_test_frame()
+      run_state()
+   else:
+      if select_exp_option.get() == "" and select_comport_option.get() == "":
+         info_message = "โปรดเช็คไฟล์ทดสอบและคอมพอร์ต"
+      elif select_exp_option.get() == "" and select_comport_option.get() != "":
+         info_message = "โปรดเช็คไฟล์ทดสอบ"
+      elif select_exp_option.get() != "" and select_comport_option.get() == "":
+         info_message = "โปรดเช็คคอมพอร์ต"
+      tk.messagebox.showinfo(title="ไม่สามารถทดสอบได้",message=info_message)
 
 
 def run_state():
@@ -102,8 +137,9 @@ def run_state():
    global start_time
    global timeout_ms
    global timer_display
-   logging.debug(main_state)
+   global serial_port
    if main_state == 0:
+      serial_port = serial.Serial(port=select_comport_option.get(),baudrate=9600,bytesize=8,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE,timeout=1)
       main_window.after(1000,run_state)
       message_label.set("การทดสอบจะเริ่มใน \n 5 วินาที")
       main_state = 1
@@ -133,7 +169,7 @@ def run_state():
       main_state = 6
       
    elif main_state == 6:
-      main_window.after(50,run_state)
+      main_window.after(10,run_state)
       if len(test_param) > 0:
          current_color,current_text = test_param[0].strip().split(",")
          test_frame.config(background=current_color)
@@ -142,6 +178,8 @@ def run_state():
          message_label.set(current_text)
          test_param.pop(0)
          start_time = current_millis()
+         serial_port.flushInput()
+         serial_port.flushOutput()
          main_state = 7
       else:
          main_state = 8
@@ -151,9 +189,10 @@ def run_state():
       if (current_millis()-start_time) >= timeout_ms:
          # record timeout
          main_state = 6
-      # if serial.available():
+      if serial.inWaiting():
+         pass
       #  process data
-      main_window.after(50,run_state)
+      main_window.after(10,run_state)
    elif main_state == 8:
       pass
    
@@ -170,7 +209,9 @@ config_frame = customtkinter.CTkFrame(home_frame,width=200,height=200,corner_rad
 config_frame.place(relx=0.5, rely=0.5,anchor=tk.CENTER)
 
 select_exp_label = customtkinter.CTkLabel(config_frame,text="เลือกการทดสอบ",font=main_thai_font,fg_color="#FFFFFF")
+select_comport_label = customtkinter.CTkLabel(config_frame,text="เลือกคอมพอร์ต",font=main_thai_font,fg_color="#FFFFFF")
 select_exp_option = customtkinter.CTkOptionMenu(master=config_frame, values=[], variable=select_exp_var,font=main_eng_font)
+select_comport_option = customtkinter.CTkOptionMenu(master=config_frame, values=[], variable=select_comport_var,font=main_eng_font)
 
 if len(exp_selectable) > 0:
    select_exp_option.configure(values=exp_selectable)
@@ -179,8 +220,9 @@ start_button = customtkinter.CTkButton(master=config_frame, text="เริ่�
 
 select_exp_label.grid(row=0,column=0,sticky=tk.W,padx=(50,10),pady=(50,0))
 select_exp_option.grid(row=0,column=1,sticky=tk.W,padx=(0,50),pady=(50,0))
-start_button.grid(row=1,column=0,columnspan=2,pady=(20,50))
-
+select_comport_label.grid(row=1,column=0,padx=(50,10),pady=20,sticky=tk.W)
+select_comport_option.grid(row=1,column=1,sticky=tk.W,padx=(0,50),pady=20)
+start_button.grid(row=2,column=0,columnspan=2,pady=(0,50))
 
 #======== test frame ===============================================================
 test_label = tk.Label(master=test_frame,textvariable = message_label,font=test_font,background='#FFFFFF')
@@ -192,5 +234,10 @@ show_home_frame()
 #============== add events =======================================================
 main_window.bind('<Escape>', lambda e: main_window.destroy())
 
+# =========== initial widgets =================
+list_available_port = list_serial_ports()
+if len(list_available_port)<=0:
+   list_available_port = ['']
+select_comport_option['values'] = list_available_port
 
 main_window.mainloop()
